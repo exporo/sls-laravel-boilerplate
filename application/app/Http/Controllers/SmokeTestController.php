56ce6e4f09c6f488101155fa768220ae80e96498
henriklippke <h.lippke@exporo.com>
2019-08-31 22:05:39 +0100

    feature/smoke-test

---
+++ application/app/Http/Controllers/SmokeTestController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use App\CountDb;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Storage;
+
+class SmokeTestController extends Controller
+{
+    protected $guzzle;
+
+    public function __construct(Client $client)
+    {
+        $this->guzzle = $client;
+    }
+
+    public function index()
+    {
+        foreach (get_class_methods($this) as $method) {
+            if (substr($method, 0, 4) === 'test') {
+                $this->{$method}();
+            }
+        }
+    }
+
+    protected function testIfDatabaseIsAccessible()
+    {
+        try {
+            CountDb::get()->count();
+        } catch (Exception $e) {
+            abort(503, __FUNCTION__);
+        }
+    }
+
+    protected function testIfStorageIsNotPublic()
+    {
+        $tmpFileName = 'tmp.txt';
+        Storage::put($tmpFileName, 'test');
+        $fileUrl = Storage::url($tmpFileName);
+
+        echo $fileUrl;
+
+        try {
+            $response = $this->guzzle->get($fileUrl, ['http_errors' => false]);
+        } catch (Exception $e) {
+        } finally {
+            if (isset($response) && $response->getStatusCode() === 200) {
+                abort(503, __FUNCTION__);
+            }
+        }
+    }
+
+    protected function testIfAssetsWereAccessible()
+    {
+        try {
+            $response = $this->guzzle->get(asset('exporo-tech.png'), ['http_errors' => false]);
+        } catch (Exception $e) {
+        } finally {
+            if (!isset($response) || $response->getStatusCode() !== 200) {
+                abort(503, __FUNCTION__);
+            }
+        }
+    }
+
+    protected function testIfApplicationCanAccessTheInternet()
+    {
+        try {
+            $response = $this->guzzle->get('https://google,de', ['http_errors' => false]);
+        } catch (Exception $e) {
+        } finally {
+            if (!isset($response) || $response->getStatusCode() !== 200) {
+                abort(503, __FUNCTION__);
+            }
+        }
+    }
+}
--- application/routes/web.php
+++ application/routes/web.php
@@ -12,3 +12,5 @@
 */
 
 Route::get('/', 'HomeController@index');
+
+Route::get('/smoke-test', 'SmokeTestController@index');
